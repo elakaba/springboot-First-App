bf1991b258589209ea5f4effd2a307b7de7d96e7
elaakaba <elaakaba7@gmail.com>
2024-03-27 21:40:49 +0100

    This is the first commit

---
+++ springboot-first-app/src/main/java/com/javaguides/springboot/SpringbootFirstAppApplication.java
@@ -0,0 +1,16 @@
+package com.javaguides.springboot;
+
+import org.apache.tomcat.websocket.AsyncChannelGroupUtil;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFirstAppApplication {
+
+	public static void main(String[] args) {
+
+
+		SpringApplication.run(SpringbootFirstAppApplication.class, args);
+	}
+
+}
